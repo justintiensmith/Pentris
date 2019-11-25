0917befa549c560abb29062d5ff3e5f0b9547bfa
justintiensmith <justintiensmith@gmail.com>
2019-11-25 09:51:06 -0800

    yeeet

--- Pentris Useful Stuff/1-objects-classes/sketch.js
+++ Pentris Useful Stuff/1-objects-classes/sketch.js
@@ -2,22 +2,26 @@ function setup() {
   createCanvas (1500, 500);
   background ("purple");
   U = new ulysess (200,200, "white"); //make a new ball from the Ball class and call it b.
-  // V = new vector (200,200,"white");
-  // W = new william (300, 69, "white");
-  // X = new xavier (250,40, "red");
-  // Y = new yosef (200,200, "white");
-  // Z = new zachary (200,200, "white");
+  V = new vector (200,200,"white");
+  W = new william (300, 69, "white");
+  X = new xavier (250,40, "red");
+  Y = new yosef (200,200, "white");
+  Z = new zachary (200,200, "white");
 }
 
 function draw(){
   U.drawUlysess ();
-  U.moveUlysess ();
-
-  // new vector();
-  // new william();
-  // new xavier();
-  // new yosef();
-  // new zachary();
+  // U.moveUlysess ();
+  V.drawVector();
+  // V.moveVector ();
+  W.drawWilliam();
+  // // W.moveWilliam ();
+  X.drawXavier();
+  // // X.moveXavier ();
+  Y.drawYosef();
+  // // U.moveYosef ();
+  Z.drawZachary();
+  // Z.moveZachary ();
 
   translate(940,0);
   grid();
@@ -57,100 +61,88 @@ class ulysess {
     		this.y = y;
 	}
 	drawUlysess(){
-    beginShape(ulysess);
-  vertex(250, 50);
-  vertex(300, 50);
-  vertex(300, 100);
-  vertex(350, 100);
-  vertex(350, 50);
-  vertex(400, 50);
-  vertex(400, 50);
-  vertex(400, 150);
-  vertex(250, 150);
-  vertex(250, 50);
-    endShape();
+    rect(250, 50, 50, 50);
+    rect(250, 100, 50, 50);
+    rect(300, 100, 50, 50);
+    rect(350, 100, 50, 50);
+    rect(350, 50, 50, 50);
 	}
-	moveUlysess(){
-    if (mousePressed){
-    this.x = mouseX
-    this.y = mouseY
-    }
+  if(KeyIsDown(LEFT_ARROW)){
+  drawUlysessFlip
+  }
+}
+
+class vector {
+	constructor(x,y) {
+		    this.x = x;
+    		this.y = y;
+	}
+	drawVector(){
+    rect(50, 250, 50, 50);
+    rect(50, 300, 50, 50);
+    rect(50, 350, 50, 50);
+    rect(100, 350, 50, 50);
+    rect(150, 350, 50, 50);
 	}
 }
 
+class william {
+	constructor(x,y) {
+		    this.x = x;
+    		this.y = y;
+	}
+	drawWilliam(){
+    rect(50, 50, 50, 50);
+    rect(100, 50, 50, 50);
+    rect(100, 100, 50, 50);
+    rect(150, 100, 50, 50);
+    rect(150, 150, 50, 50);
+ }
+ if(KeyIsDown(LEFT_ARROW)){
+   drawWilliamFlip(){
 
-//
-// function vector(){
-//   beginShape(vector);
-// vertex(50, 250);
-// vertex(100, 250);
-// vertex(100, 350);
-// vertex(200, 350);
-// vertex(200, 350);
-// vertex(200, 400);
-// vertex(50, 400);
-// vertex(50,250);
-//   endShape();
-// }
-//
-// function william(){
-//   beginShape(william);
-// vertex(50, 50);
-// vertex(50, 100);
-// vertex(100, 100);
-// vertex(100, 150);
-// vertex(150, 150);
-// vertex(150, 200);
-// vertex(200, 200);
-// vertex(200,100);
-// vertex(150,100);
-// vertex(150,50);
-// vertex(50,50);
-//   endShape();
-// }
-//
-// function xavier(){
-//   beginShape(xavier);
-// vertex(300, 250);
-// vertex(350, 250);
-// vertex(350, 300);
-// vertex(400, 300);
-// vertex(400, 350);
-// vertex(350, 350);
-// vertex(350, 400);
-// vertex(300, 400);
-// vertex(300, 350);
-// vertex(250, 350);
-// vertex(250, 300);
-// vertex(300, 300);
-// vertex(300, 250);
-//   endShape();
-// }
-//
-// function yosef(){
-//   beginShape(xavier);
-// vertex(450, 100);
-// vertex(550, 100);
-// vertex(550, 50);
-// vertex(600, 50);
-// vertex(600, 100);
-// vertex(650, 100);
-// vertex(650, 150);
-// vertex(450, 150);
-// vertex(450, 100);
-//   endShape();
-// }
-//
-// function zachary(){
-//   beginShape(xavier);
-// vertex(450, 250);
-// vertex(550, 250);
-// vertex(550, 350);
-// vertex(600, 350);
-// vertex(600, 400);
-// vertex(500, 400);
-// vertex(500, 300);
-// vertex(450, 300);
-// vertex(450, 250);
-//   endShape();
-// }
+   }
+ }
+}
+
+class xavier {
+	constructor(x,y) {
+		    this.x = x;
+    		this.y = y;
+	}
+	drawXavier(){
+    rect(300, 250, 50, 50);
+    rect(250, 300, 50, 50);
+    rect(300, 300, 50, 50);
+    rect(350, 300, 50, 50);
+    rect(300, 350, 50, 50);
+ }
+}
+
+class yosef {
+	constructor(x,y) {
+		    this.x = x;
+    		this.y = y;
+	}
+	drawYosef(){
+    rect(450, 100, 50, 50);
+    rect(500, 100, 50, 50);
+    rect(550, 100, 50, 50);
+    rect(550, 50, 50, 50);
+    rect(600, 100, 50, 50);
+ }
+}
+
+class zachary {
+	constructor(x,y) {
+		    this.x = x;
+    		this.y = y;
+	}
+	drawZachary(){
+    rect(450, 250, 50, 50);
+    rect(500, 250, 50, 50);
+    rect(500, 300, 50, 50);
+    rect(500, 350, 50, 50);
+    rect(550, 350, 50, 50);
+ }
+}

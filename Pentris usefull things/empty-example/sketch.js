5e8dc61bbf90f21bb156f3e4d1b5f2faae0a14c1
marcodonohoe22 <marcodonohoe@gmail.com>
2019-11-25 09:44:24 -0800

    movement 11/25

--- Pentris usefull things/empty-example/sketch.js
+++ Pentris usefull things/empty-example/sketch.js
@@ -1,25 +1,43 @@
 
-let f;
+let F;
+let I;
 
 
 function setup() {
-  createCanvas(800, 400);
-  f = new Fredrick();
+  createCanvas(1500, 500);
+  F = new Fredrick();
+  I = new Ivanka
 }
 
+clicked();{
+  let d = dist(mouseX, mouseY, this.x, this.y)
+  print ('spookey')
+}
 
 
 
 function draw(){
 	background("blue");
-    f.drawFredrick();
+    F.drawFredrick();
+    F.moveFredrick();
+    //I.drawIvanka();
+
+
+
+      let colorcheck = get (mouseX, mouseY);
+      print(colorcheck)
+
+      if (colorcheck[0]==255 && colorcheck[1]==255 && colorcheck[2]==255 && colorcheck[3]==255){
+      print ('white detected')
+    }
 }
+
 
 
 class Fredrick {
 
 	constructor(x,y,color){
-		    this.x = 200;
+		    this.x = 100;
     		this.y = 100;
         this.color=("white");
 	}
@@ -31,9 +49,28 @@ class Fredrick {
     rect(50,150,50,50);
     rect(100,200,50,50);
 	}
-	moveFredrick(){
-		// if (mousePressed)
-    this.x = mouseX();
-    this.y = mouseY();
+  // moveFredrick();{
+  //     if(mousePressed){
+  //       this.x = 100
 
-}}
+      }
+
+  }
+}
+
+class Ivanka {
+
+	constructor(x,y,color){
+		    this.x = 200;
+    		this.y = 100;
+        this.color=("white");
+	}
+	drawIvanka(){  // draw a ball on the screen at x,y
+      fill(this.color);
+      rect(250,100,50,50);
+      rect(250,150,50,50);
+      rect(250,200,50,50);
+      rect(250,250,50,50);
+      rect(250,300,50,50);
+    }
+	}

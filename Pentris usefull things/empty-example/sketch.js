7e95474c128393ca688d9bb1239b568a0e8e75b1
marcodonohoe22 <marcodonohoe@gmail.com>
2019-11-30 17:29:52 -0800

    IDK
    stuck

--- Pentris usefull things/empty-example/sketch.js
+++ Pentris usefull things/empty-example/sketch.js
@@ -41,7 +41,8 @@ class Fredrick {
     		this.y = 100;
         this.color=("white");
 	}
-	drawFredrick(){  // draw a ball on the screen at x,y
+
+	drawFredrick(){
     fill(this.color)
     rect(100,100,50,50);
     rect(150,100,50,50);
@@ -49,14 +50,8 @@ class Fredrick {
     rect(50,150,50,50);
     rect(100,200,50,50);
 	}
-  // moveFredrick();{
-  //     if(mousePressed){
-  //       this.x = 100
-
-      }
-
-  }
 }
+
 
 class Ivanka {
 

0149fe99380ba1f6035a178a6239b3945c595b86
justintiensmith <justintiensmith@gmail.com>
2019-12-02 16:49:07 -0800

    yote

--- Pentris Useful Stuff/1-objects-classes/sketch.js
+++ Pentris Useful Stuff/1-objects-classes/sketch.js
@@ -1,99 +1,86 @@
-let rotation = 0
-
 function setup() {
-  angleMode(DEGREES)
-  createCanvas (1500, 500);
-  background ("purple");
-  U = new ulysess ();
-  V = new vector ();
-//   W = new william (300, 69, "white");
-//   X = new xavier (250,40, "red");
-//   Y = new yosef (200,200, "white");
-//   Z = new zachary (200,200, "white");
+  createCanvas(1440, 500);
+  background("purple");
+  U = new ulysess(200, 200, "white");
+  // V = new vector (200,200, "white");
+  // W = new william (200,200, "white");
+  // X = new xavier (200,200, "white");
+  // Y = new yosef (200,200, "white");
+  // Z = new zachary (200,200, "white");
 }
+
+function draw(){
+  U.drawUlysess ();
+  // V.drawVector ();
+  // W.drawWilliam ();
+  // X.drawXavier ();
+  // Y.drawYosef ();
+  // Z.drawZachary ();
+}
+
+// function keyPressed() {
+// if(keyIsDown(39)){
+//   background("purple");
+//   a += 50
+//   }
+// }
+//
+// function keyPressed() {
+// if(keyIsDown(40)){
+//   background("purple");
+//   b += 50
+//   }
+// }
+
 function keyPressed() {
-if(keyIsDown(32)){
-  rotation += 90
+if(keyIsDown(37)){
+  background("purple");
+  c += 50
   }
 }
-function draw(){
-  background("purple")
-  U.drawUlysess (200,200,rotation);
-  V.drawVector(200,200, rotation);
-  // V.moveVector ();
-  // W.drawWilliam();
-  // W.moveWilliam ();
-  // X.drawXavier();
-  // X.moveXavier ();
-  // Y.drawYosef();
-  // U.moveYosef ();
-  // Z.drawZachary();
-  // Z.moveZachary ();
-//
-//   translate(940,0);
-//   grid();
-//
-//   let colorcheck = get (mouseX, mouseY);
-//   print(colorcheck)
-//
-//   if (colorcheck[0]==255 && colorcheck[1]==255 && colorcheck[2]==255 && colorcheck[3]==255){
-//   print ('it works')
+
+// function keyPressed() {
+// if(keyIsDown(38)){
+//   background("purple");
+//   d += 0
+//   }
 // }
-}
-//
-// function grid() {
-//   line(50, 50, 50, 450);
-//   line(100, 50, 100, 450);
-//   line(150, 50, 150, 450);
-//   line(200, 50, 200, 450);
-//   line(250, 50, 250, 450);
-//   line(300, 50, 300, 450);
-//   line(350, 50, 350, 450);
-//   line(400, 50, 400, 450);
-//   line(450, 50, 450, 450);
-//   line(50, 50, 450, 50);
-//   line(50, 100, 450, 100);
-//   line(50, 150, 450, 150);
-//   line(50, 200, 450, 200);
-//   line(50, 250, 450, 250);
-//   line(50, 300, 450, 300);
-//   line(50, 350, 450, 350);
-//   line(50, 400, 450, 400);
-//   line(50, 450, 450, 450);
-// }
+
+let a = 0
+let b = 0
+let c = 0
+let d = 0
 
 class ulysess {
-	constructor() {
-	}
-	drawUlysess(x,y,r){
-    push();
-    translate (x,y);
-    rotate (r);
-    rect(-75, -50, 50, 50, 5,5,0,0);
-    rect(-75, 0, 50, 50, 0,0,0,5);
-    rect(-25, 0, 50, 50);
-    rect(25, 0, 50, 50,0,0,5,0);
-    rect(25, -50, 50, 50,5,5,0,0);
-    pop();
-	}
+  constructor(x,y) {
+        this.x = x;
+        this.y = y;
+  }
+  drawUlysess(){
+  rect(50 + a + a, 50 + b + b, 50, 50);
+  rect(50 + a, 100 - a + c, 50, 50);
+  rect(100, 100, 50, 50);
+  rect(150 - a - c, 100 + a + c, 50, 50);
+  rect(150 - c, 50 + a + a + b + b, 50, 50);
+   }
 }
-
-class vector {
-	constructor() {
-	}
-	drawVector(x,y,r){
-    translate (200,0)
-    push();
-    translate (x,y);
-    rotate (r);
-    rect(-75, -75, 50, 50);
-    rect(-75, -25, 50, 50);
-    rect(-75, 25, 50, 50);
-    rect(-25, 25, 50, 50);
-    rect(25, 25, 50, 50);
-    pop();
-	}
-}
+//
+// class vector {
+// 	constructor() {
+// 	}
+// 	drawVector(x,y,r){
+//     translate (200,0)
+//     push();
+//     translate (x,y);
+//     rotate (r);
+//     rect(-75, -75, 50, 50);
+//     rect(-75, -25, 50, 50);
+//     rect(-75, 25, 50, 50);
+//     rect(-25, 25, 50, 50);
+//     rect(25, 25, 50, 50);
+//     pop();
+// 	}
+// }
 //
 // class william {
 // 	constructor(x,y) {
